106de7b1250235d8322a2b420567b09a9515b80d
Luiz Fernando <luizfcneto123@gmail.com>
2017-04-23 18:02:13 -0300

    *alterando (*var).campo com
    para ->, +adicionando em .c

--- ListaDinamicaEncadeada/lista2.c
+++ ListaDinamicaEncadeada/lista2.c
@@ -7,10 +7,43 @@ Nome: Luiz Fernando
 */
 
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "lista2.h"
 
-int main(){
-	telemento *lista;
+int main(void) {
+	tElementoDaListaEncadeada *lista;
+	taluno *dados;
+	int posicao = 5;
 
+	criaLista(lista);
+	printaListaEncadeada(lista);
+
+	dados->matricula = 111111;
+	adicionaElementoFinal(lista, dados);
+	dados->matricula = 111112;
+	adicionaElementoFinal(lista, dados);
+	dados->matricula = 111113;
+	adicionaElementoFinal(lista, dados);
+	dados->matricula = 111114;
+	adicionaElementoFinal(lista, dados);
+	dados->matricula = 111116;
+	adicionaElementoFinal(lista, dados);
+	dados->matricula = 111117;
+	adicionaElementoFinal(lista, dados);
+	dados->matricula = 111118;
+	adicionaElementoFinal(lista, dados);
+	dados->matricula = 111119;
+	adicionaElementoFinal(lista, dados);
+
+	printaListaEncadeada(lista);
+
+	dados->matricula = 111110;
+	adicionaElementoInicio(lista, dados);
+	printaListaEncadeada(lista);
+
+	adicionaElementoPosicao(lista, dados, posicao);
+	printaListaEncadeada(lista);
+
+
+	return 0;
 }
--- ListaDinamicaEncadeada/lista2.h
+++ ListaDinamicaEncadeada/lista2.h
@@ -3,7 +3,9 @@
 2-	Quando um ponteiro de um elemento aponta para nulo, quer dizer que esse elemento é o ultimo.
 
 Notação: relep = ponteiro de um ponteiro <=> (*relep) Inicio do elemento <=> (*relep).proximo => segundo elemento(que é um ponteiro)
+‘->’ = '(*variavel).'
 */
+
 
 //Para fazer algo simples e demonstrativo irei deixar a estrutura aluno apenas com matricula
 typedef struct tAluno{
@@ -13,7 +15,7 @@ typedef struct tAluno{
 	//float nota1, nota2, nota3, media;
 }taluno;
 
-typedef struct tElementoDaListaEncadeada{
+typedef struct tElementoDaListaEncadda{
 	tElementoDaListaEncadeada proximo*;	//é um ponteiro para o mesmo tipo de variavel
 	taluno info; 	//(*taluno).info.matricula
 }*tElementoDaListaEncadeada;
@@ -23,19 +25,19 @@ tElementoDaListaEncadeada* criaLista(tElementoDaListaEncadeada* referenciaElemen
 	tElementoDaListaEncadeada listaEncadeada;
 	listaEncadeada = (tElementoDaListaEncadeada*)malloc(sizeof(tElelementoDaListaEncadeada));
 	if(listaEncadeada != NULL){
-		listaEncadeada* = NULL;	//*Tenho que devolver o primeiro ponteiro com conteudo de onde aponta pra Nulo;
+		*listaEncadeada = NULL;	//*Tenho que devolver o primeiro ponteiro com conteudo de onde aponta pra Nulo;
 	}
 
 	return listaEncadeada;
 }
 
-//Libera o espaço utilizado pela lista
+//Libera o espaço utilizado pela lista 
 void liberaListaEncadeada(tElelementoDaListaEncadeada* referenciaElementoDaListaEncadeada){
 	if(referenciaElementoDaListaEncadeada != NULL){	//Se o ponteiro estiver apontando pra alguem
 		tElementoDaListaEncadeada noh*;		//noh é auxiliar
-		while( (*referenciaElementoDaListaEncadeada) != NULL){	//Enquanto eu tenho conteudo na lista
-			noh = (*referenciaElementoDaListaEncadeada);
-			(*referenciaElementoDaListaEncadeada) = (*referenciaElementoDaListaEncadeada).proximo;
+		while( *referenciaElementoDaListaEncadeada != NULL){	//Enquanto eu tenho conteudo na lista
+			noh = *referenciaElementoDaListaEncadeada;
+			*referenciaElementoDaListaEncadeada = referenciaElementoDaListaEncadeada->proximo;	//Aqui não tem problema eu usar a referencia pra percorrer a lista pois estarei eliminando toda a lista.
 			free(noh);
 		}
 		free(referenciaElementoDaListaEncadeada);
@@ -47,29 +49,37 @@ void adicionaElementoFinal(tElementoDaListaEncadeada* referenciaElementoDalistaE
 	//Caso não tenha ponteiro apontando para o primeiro ponteiro
 	if( referenciaElementoDaListaEncadeada == NULL ){
 		puts("ERRO - Lista Invalida! \n");
-	}
-	tElementoDaListaEncadeada noh* = (tElementoDaListaEncadeada*)malloc(sizeof(tElementoDaListaEncadeada));	//crio um ponteiro para um novo elemento
+	}else{
+		tElementoDaListaEncadeada *noh;
+		noh = (tElementoDaListaEncadeada*)malloc(sizeof(tElementoDaListaEncadeada));	//crio um ponteiro para um novo elemento
 
-	//Verifica se um novo elemento(noh) foi criado
-	if( noh == NULL){
-		puts("ERRO - Lista Invalida! \n");
-	}
-	(*noh).info = (*rap).matricula;
-	(*noh).proximo = NULL;
+		//Verifica se um novo elemento(noh) foi criado
+		if( noh == NULL){
+			puts("ERRO - Lista Invalida! \n");
+		
+		}else{
+			noh->info = rap->matricula;
+			noh->proximo = NULL;
 
-	//Verifica se o inicio é vazio
-	if( (*referenciaElementoDaListaEncadeada) == NULL ){
-		(*referenciaElementoDaListaEncadeada) = noh;		//caso seja, vai apontar pro primeiro elemento(que eu adicionei)
-	}
+			//Verifica se o inicio é vazio
+			if( *referenciaElementoDaListaEncadeada == NULL ){
+			
+				*referenciaElementoDaListaEncadeada = noh;		//caso seja, vai apontar pro primeiro elemento(que eu criei: noh)
+			}else{
 
-	//caso ja exista ponteiro apontando para elementos:
-	tElementoDaListaEncadeada aux* = (tElementoDaListaEncadeada*)malloc(sizeof(tElementoDaListaEncadeada));	//crio um outro ponteiro de elemento para me auxiliar
-	aux = (*referenciaElementoDaListaEncadeada);	// aponta para o primeiro elemento
+				//caso ja exista ponteiro apontando para elementos:
+				tElementoDaListaEncadeada *aux = (tElementoDaListaEncadeada*)malloc(sizeof(tElementoDaListaEncadeada));	//crio um outro ponteiro de elemento para me auxiliar
+				aux = *referenciaElementoDaListaEncadeada;	// aponta para o primeiro elemento
 
-	while( (*aux).proximo != NULL ){
-		aux = (*aux).proximo;	//
+				while( aux->proximo != NULL ){
+					aux = aux->proximo;	//
+				}
+				aux->proximo = noh;
+					
+			}
+
+		}
 	}
-	(*aux).proximo = noh;
 }
 
 //Adiciono um elemento novo no inicio
@@ -86,13 +96,13 @@ void adicionaElementoInicio(tElementoDaListaEncadeada* referenciaElementoDaLista
 		puts("ERRO - Lista Invalida! \n");
 	}
 	//Funciona tanto para como o primeiro elemento a ser criado, como para caso ja exista elemento nessa lista
-	(*noh).info = (*rap).matricula;
-	(*noh).proximo = (*referenciaElementoDaListaEncadeada);	//nó novo aponta pro endereço que apontava pro primeiro(que agora sera o segundo)
-	(*referenciaElementoDaListaEncadeada) = noh;	//endereço do inicio aponta pro novo primeiro noh
+	noh->info = rap->matricula;
+	noh->proximo = *referenciaElementoDaListaEncadeada ;	//nó novo aponta pro endereço que apontava pro primeiro(que agora sera o segundo)
+	*referenciaElementoDaListaEncadeada = noh;	//endereço do inicio aponta pro novo primeiro noh
 }
 
 //adiciono um elemento novo em qualquer lugar
-void adicionaElementoPosicao(tElementoDaListaEncadeada* referenciaElementoDaListaEncadeada, taluno rap ,int posicao){
+void adicionaElementoPosicao(tElementoDaListaEncadeada* referenciaElementoDaListaEncadeada, taluno* rap ,int posicao){
 	if(tamanhoListaEncadeada(referenciaElementoDaListaEncadeada) > posicao || tamanhoListaEncadeada(referenciaElementoDaListaEncadeada) < 0 ){
 		puts("ERRO - Não existe essa posicao!\n");
 	}
@@ -106,25 +116,25 @@ void adicionaElementoPosicao(tElementoDaListaEncadeada* referenciaElementoDaList
 		puts("ERRO - Elemento noh Não Criado Com Sucesso \n");
 	}else{
 		
-		noh = (*referenciaElementoDaListaEncadeada);
-		(*noh).info = (*rap).matricula;
+		noh = *referenciaElementoDaListaEncadeada;
+		noh->info = rap->matricula;
 
 		tElementoDaListaEncadeada *aux = (tElementoDaListaEncadeada*)malloc(sizeof(tElementoDaListaEncadeada));	//saber qual elemento vai apontar para o noh inserido
 		if( aux == NULL){
 			puts("ERRO - Elemento aux Não Criado Com Sucesso \n ");
 
 		}else{
-			aux = (*referenciaElementoDaListaEncadeada);
+			aux = *referenciaElementoDaListaEncadeada;
 			
 			while( i != posicao){	//Faço ele percorrer a lista até encontrar posicao
-				noh = (*noh).proximo;	// noh recebe o primeiro elemento da lista encadeada posicao[quando i = 0]
+				noh = noh->proximo;	// noh recebe o primeiro elemento da lista encadeada posicao[quando i = 0]
 				if( posicao - i > 0){	//quando (i == posicao) = 0 pararei de passar para o proximo
-					aux = (*aux).proximo;	//[final] elemento vai ser o antecessor ao noh	
+					aux = aux->proximo;	//[final] elemento vai ser o antecessor ao noh	
 				}
 				i++;
 			}
-			(*aux).proximo = noh;
-			noh = (*noh).proximo;
+			aux->proximo = noh;
+			noh = noh->proximo;
 		}
 	}
 }
@@ -144,31 +154,31 @@ void removeElementoPorIndice(tElementoDaListaEncadeada* referenciaElementoDaList
 			
 				tElementoDaListaEncadeada *noh;
 				noh = (tElementoDaListaEncadeada*)malloc(sizeof(tElementoDaListaEncadeada));
-				noh = (*referenciaElementoDaListaEncadeada);
+				noh = *referenciaElementoDaListaEncadeada;
 				
-				if(tElementoDaListaEncadeada == NULL){
+				if(noh == NULL){
 					puts("ERRO - Elemento noh Não Foi Criado Com Sucesso! \n");
 			
 				}else{
 					int i = 0;
 					tElementoDaListaEncadeada *aux;
 					aux = (tElementoDaListaEncadeada*)malloc(sizeof(tElementoDaListaEncadeada));
-					aux = (*referenciaElementoDaListaEncadeada);
+					aux = *referenciaElementoDaListaEncadeada;
 					if(aux == NULL){
 						puts("ERRO - Elemento aux Não Foi Criado Com Sucesso! \n");
 					
 					}else{
 
 						while( indice != i ){
-							noh= = (*noh).proximo;
+							noh = noh->proximo;
 						
 							if( i == indice-1){
-								aux = (*aux).proximo 	//final aux = i-1 e noh = i -> aux é anterior a noh
+								aux = aux->proximo 	//final aux = i-1 e noh = i -> aux é anterior a noh
 							}
 							i++;	
 						}
 
-						(*aux).proximo = (*noh).proximo;	//o campo do endereço proximo de aux vai ser o que era o proximo de noh
+						aux->proximo = noh->proximo;	//o campo do endereço proximo de aux vai ser o que era o proximo de noh
 						liberaListaEncadeada(noh);	
 					}
 				}
@@ -185,20 +195,21 @@ void removeElementoPorConteudo(tElementoDaListaEncadeada referenciaElementoDaLis
 		noh = (tElementoDaListaEncadeada*)malloc(sizeof(tElementoDaListaEncadeada));
 		tElementoDaListaEncadeada *aux;
 		aux = (tElementoDaListaEncadeada*)malloc(sizeof(tElementoDaListaEncadeada));
-		noh = (*referenciaElementoDaListaEncadeada);
-		aux = (*referenciaElementoDaListaEncadeada);
+		noh = *referenciaElementoDaListaEncadeada;
+		aux = *referenciaElementoDaListaEncadeada;
+		
 		if(aux == NULL || noh == NULL || referenciaElementoDaListaEncadeada == NULL){
 			puts("ERRO - Algum elemento com erro de alocacao! \n");
 
 		}else{
-			while( (*noh).proximo == NULL ){
-				noh = (*noh).proximo;
-				if( (*noh).info == (*rap).matricula ){
+			while( noh->proximo == NULL ){
+				noh = noh->proximo;
+				if( noh->info == rap->matricula ){
 					tem = 1;
-					aux = (*noh).proximo;	//aux vai ser antecessor a noh , pois só será apontado para o proximo 
+					aux = noh->proximo;	//aux vai ser antecessor a noh , pois só será apontado para o proximo 
 				}
 				if(tem != 1){
-					aux = (*aux).proximo;
+					aux = aux->proximo;
 				}
 			}
 			if(tem == -1){
@@ -227,14 +238,14 @@ void buscaPorElemento(tElementoDaListaEncadeada* referenciaElementoDaListaEncade
 		
 		}else{
 			int i = 0;
-			noh = (*referenciaElementoDaListaEncadeada);
+			noh = *referenciaElementoDaListaEncadeada;
 
 			while( i != indice ){
-				noh = (*noh).proximo;
+				noh = noh->proximo;
 				i++;
 			}
-			noh = (*noh).proximo;
-			printf("	[%d] - [ %d ]  \n",i,(*noh).info);
+			noh = noh->proximo;
+			printf("	[%d] - [ %d ]  \n",i, noh->info);
 		}
 	}
 }
@@ -248,12 +259,12 @@ void buscaPorConteudo(tElementoDaListaEncadeada* referenciaElementoDaListaEncade
 	if( referenciaElementoDaListaEncadeada == NULL || noh == NULL ){
 		puts("ERRO - Algum Elemento Invalido! \n");
 	}else{
-		noh = (*tElementoDaListaEncadeada);
+		noh = *referenciaElementoDaListaEncadeada;
 
-		while( (*noh).proximo != NULL ){
+		while( noh->proximo != NULL ){
 			
-			if( (*noh).info == matriculaV ){
-				printf(" [%d] - [ %d ] \n", i, (*noh).info);
+			if( noh->info == matriculaV ){
+				printf(" [%d] - [ %d ] \n", i, noh->info);
 				existe = 1;
 			}
 			i++;
@@ -266,17 +277,17 @@ void buscaPorConteudo(tElementoDaListaEncadeada* referenciaElementoDaListaEncade
 
 }
 
-//Tamanho da lista
+//Tamanho da list
 int tamanhoListaEncadeada(tElementoDaListaEncadeada* referenciaElementoDaListaEncadeada){
 	if( referenciaElementoDaListaEncadeada == NULL){	//Verifica se a Lista é valida!
 		return 0;
 	}
 	int contador = 0;
-	tElementoDaListaEncadeada noh*;
-	noh = *referenciaElementoDaListaEncadeada;	//Que conteudo de relep? proximo ou info? R: o conteudo desse ponteiro é um outro ponteiro.
+	tElementoDaListaEncadeada *noh;
+	noh = *referenciaElementoDaListaEncadeada;	/*Que conteudo de referenciaElementoDaListaEncadeada? proximo ou info? O conteudo desse ponteiro é um outro ponteiro. */
 	while( noh != NULL ){
 		contador++;
-		noh = (*noh).proximo;
+		noh = noh->proximo;
 	}
 	return contador;
 }
@@ -289,7 +300,7 @@ int listaEncadeadaVazia(tElementoDaListaEncadeada* referenciaElementoDaListaEnca
 	if(referenciaElementoDaListaEncadeada == NULL){	//se o ponteiro não esta apontando pra nenhum outro endereço lista está vazia(true)
 		return 1;
 	}
-	if((*referenciaElementoDaListaEncadeada) == NULL){
+	if( *referenciaElementoDaListaEncadeada == NULL){
 		return 1;		//se o ponteiro não esta apontando pra nenhum conteudo lista está vazia(true)
 	}
 	return 0;
@@ -304,11 +315,12 @@ void printaListaEncadeada(tElementoDaListaEncadeada* referenciaElementoDaListaEn
 	if( noh == NULL ){
 		puts("ERRO - Elemento noh Não Criado Com Sucesso! \n");
 	}else{
-		while((*noh).proximo != NULL){
-			prinf("Indice:	[%d] Ponteiro: [ %p ] --> [ %d ] (Conteudo)  \n",i, noh, (*noh).info);
-			i++;
+		while( noh->proximo != NULL){
+			printf("Indice:	[%d] Ponteiro: [ %p ] --> [ %d ] (Conteudo)  \n",indice, noh, noh->info );
+			indice++;
 		}
 
 	}
+	printf("_____________________________________________________________________________________________________");
 
 }
