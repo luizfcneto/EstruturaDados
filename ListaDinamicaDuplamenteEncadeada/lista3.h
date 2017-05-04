1c7435f14915dfe5a6f223687cc9fbcf61924fe7
Luiz Fernando <luizfcneto123@gmail.com>
2017-05-03 23:29:09 -0300

    *Busca implementadas, e outras funcoes

--- ListaDinamicaDuplamenteEncadeada/lista3.h
+++ ListaDinamicaDuplamenteEncadeada/lista3.h
@@ -31,14 +31,14 @@ tRaiz* criaLista ( tRaiz* referenciaRaiz )
 	listaNova = (tRaiz*) malloc ( sizeof ( tRaiz ) );
 	if ( listaNova == NULL ) printf("ERRO - Lista Nova Invalida! \n");
 	return listaNova;
-}
+}	
 
 //Liberar Lista
 void liberaLista ( tRaiz* referenciaRaiz )
 {
 	if ( referenciaRaiz != NULL )
 	{
-		tRaiz *noh;		//noh vai armazenar cada elemento da lista(*referenciaRaiz) e referenciaRaiz vai percorrer a lista
+		tRaiz *noh;		//noh vai armazenar cada elemento da lista(*referenciaRaiz) e referenciaRaiz vai percorrer a lista 
 		noh = *referenciaRaiz;
 		while ( referenciaRaiz != NULL ){
 			noh = *referenciaRaiz;
@@ -54,57 +54,37 @@ void liberaLista ( tRaiz* referenciaRaiz )
 //Tamanho Lista
 int tamanhoLista ( tRaiz* referenciaRaiz )
 {
-
+	int indice = 0;
+	if ( referenciaRaiz != NULL )	{
+		tElemento *noh;
+		noh = ( tElemento* ) malloc ( sizeof ( tElemento ) );
+		if ( noh == NULL )	printf ("ERRO - Noh nao criado com Sucesso! \n");
+			while ( noh != NULL ) {
+				indice++;
+			}
+	} else {
+		puts ("ERRO - Lista Invalida! \n");
+	}
+	return indice;
 }
 
 //Lista Vazia
 int listaVazia ( tRaiz* referenciaRaiz )
 {
-
+	if ( referenciaRaiz == NULL ) return 1;
+	if ( *referenciaRaiz == NULL ) return 1;
+	else return 0;
 }
 
 //Lista Cheia -> Não faz sentido em listas dinamicas, o limite de alocação é a quantidade de memoria disponivel pra rodar o programa
 
 //Adiciona na Lista Ordenado
-void adicionaOrdenadoLista ( tRaiz* referenciaRaiz, taluno *rap )
+void adicionaOrdenadoLista ( tRaiz* referenciaRaiz )
 {
-	if ( referenciaRaiz != NULL ){
-
-		//ListaVazia
-		if ( *referenciaRaiz == NULL ) {
-				tElemento *novo;
-				novo = *referenciaRaiz;
-				novo->anterior = NULL;
-				novo->proximo = NULL;
-				novo->info.matricula = rap->matricula;
-		}	else {
-			tElemento *noh, *novo;
-			noh = *referenciaRaiz;
-			novo = *referenciaRaiz;
-			printf ("Elemento: [ %f ] adicionado ordenadamente com sucesso!! \n", rap->matricula);
-			while( noh == NULL)	{
-					noh->anterior = noh;
-					noh = noh->proximo;
-
-					if ( (*noh).anterior->info.matricula < rap->matricula && rap->matricula < (*noh).proximo->info.matricula ){
-						novo->info.matricula = rap->matricula;
-						novo->anterior = noh;
-						novo->proximo = noh->anterior;
-						break;
-					}
-			}if ( noh == NULL ) {		//Se entrar aqui percorreu toda a lista, será o ultimo elemento a ser adicionado
-				noh->proximo = novo;
-				novo->anterior = noh;
-				novo->proximo = NULL;
-			}
-
-		}
-	}
-
 
 }
 
-//Adiciona no final da Lista
+//Adiciona no final da Lista 
 void adicionaFinalLista ( tRaiz* referenciaRaiz )
 {
 
@@ -133,38 +113,75 @@ void removerConteudoLista ( tRaiz* referenciaRaiz, taluno* rap )
 //Busca por indice
 void buscaPosicaoLista ( tRaiz* referenciaRaiz, int indice )
 {
-
+	if ( referenciaRaiz != NULL ) {
+		if ( indice > tamanhoLista ( referenciaRaiz ) || indice < 0 )	puts ("ERRO - Indice Invalido, menor ou maior que tamanho da Lista!\n");
+		else { 
+			tElemento *noh;
+			noh = ( tElemento* ) malloc ( sizeof ( tElemento ) );
+			if ( noh == NULL )	puts ("ERRO - Noh nao criado com sucesso! \n");
+			else {
+				int i = 0;
+				while ( noh != NULL ){
+					if ( i == indice ) {
+						printf ("Indice: [ %d ] \tConteudo (Matricula): [ %d ] \n", indice, noh->info.matricula );
+						break;
+					}
+					i++;
+					noh = noh->proximo;
+				}
+			}
+		}
+	} else {
+		printf ("ERRO - Lista Invalida! \n");
+	}
 }
 
 //Busca por Conteudo [matricula]
 void buscaConteudoLista ( tRaiz* referenciaRaiz, taluno* rap )
 {
 	if ( referenciaRaiz != NULL ) {
-			if ( *referenciaRaiz )	printf ("ERRO - LISTA VAZIA!! \n");
-			else	{
-					int indice = 0, tem = -1;
-					tElemento *noh;
-					noh = *referenciaRaiz;
-
-					while ( noh == NULL )	{
-						if ( noh->info.matricula == rap->matricula )	{
-							printf ("Indice [ %d ] Endereço anterior: [ %p ] <-- Conteudo: [ %d ]  --> Endereço Proximo: [ %p ] ",indice, noh->anterior, noh->info.matricula, noh->proximo );
-							tem = 1;
-							break;
-						}
-						noh->anterior = noh;
-						noh = noh->proximo;
-						indice++;
-					}	if ( tem == -1 ) printf ("ERRO - Elemento [ %d ] não encontrado na Lista! \n", rap->matricula);
-
+		tElemento *noh;
+		if ( listaVazia ( referenciaRaiz ) != 1 ){
+			noh = ( tElemento* ) malloc ( sizeof( tElemento ) );
+			if ( noh == NULL ) 	puts ("ERRO - Noh nao criado com Sucesso! \n");
+			else {
+				int tem = -1, indice = 0;
+				while ( noh != NULL ) {
+					if ( noh->info.matricula == rap->matricula ) {
+						tem = 1;
+						printf ("Indice: [ %d ] \tConteudo (Matricula): [ %d ] \n", indice, noh->info.matricula );
+						break;
+					}
+					noh = noh->proximo;
+					indice++;
+				}
+				if ( tem == -1 )	puts ("ERRO - Elemento nao consta na Lista! \n");
 			}
-
-	}else printf ("ERRO - LISTA INVALIDA! \n");
+		} else puts ("ERRO - Lista Vazia! \n");
+	} else {
+		puts ("ERRO - Lista Invalida! \n");
+	}
 
 }
 
 //Printa lista na tela
 void printaListaDuplamenteEncadeada ( tRaiz* referenciaRaiz )
 {
-
+	if ( referenciaRaiz != NULL )	{
+		tElemento *noh;
+		noh = ( tElemento* ) malloc ( sizeof ( tElemento ) );
+		if( noh == NULL)	printf ("ERRO - Noh nao criado com Sucesso! \n");
+		else {
+			noh = *referenciaRaiz;
+			int indice = 0;
+			while ( noh != NULL )	{
+				printf (" Indice: [ %d ] \tEndereco: [ %p ] \tConteudo (Matricula): [ %d ] \tAponta: [ %p ] \n", indice, noh, noh->info.matricula, noh->proximo );
+				indice++;
+				noh = noh->proximo;
+			}
+		}
+		printf ("__________________________________________________________________________________________________________________  \n");
+	} else {
+		puts ( "ERRO - Lista Invalida! \n");
+	}
 }
