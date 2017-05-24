4dad3b2c0ecbc167454a0d043d841e30c8f6781d
Luiz Fernando <luizfcneto123@gmail.com>
2017-05-23 22:03:18 -0300

    +Começando Filas (estatica/dinamica)

---
+++ Filas/filaEstatica.c
@@ -0,0 +1,23 @@
+/*				Fila Estatica 
+
+Descrição:
+	Fila é uma lista encadeada que só podemos inserir e excluir elementos nas extremidades apenas e por sua vez não podemos fazer busca.
+	
+
+Nome: Luiz Fernando (luizfcneto)
+*/
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "lista4.h"
+
+int main()
+{
+
+
+	return 0;
+}
---
+++ Filas/filaEstatica.h
@@ -0,0 +1,4 @@
+/*
+Implementação da Fila Estática
+
+*/
