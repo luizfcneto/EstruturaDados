c53ca6d9e0a268a440e73bebed377d9250c70597
Luiz Fernando <luizfcneto123@gmail.com>
2017-05-23 22:17:30 -0300

    *Declarando as funcoes

--- Filas/filaEstatica.h
+++ Filas/filaEstatica.h
@@ -2,3 +2,47 @@
 Implementação da Fila Estática
 
 */
+#define MAX 10
+
+//Estrutura Aluno
+typedef struct tAluno
+{
+	int matricula;
+	// char nome[50];
+	// float nota;
+
+} taluno;
+
+// Estrutura Fila
+typedef struct tFila
+{
+	int inicio, final, quantidade;
+	taluno dado[MAX];
+
+} tfila;
+
+
+//Cria Fila
+tfila* criaFila ( ) 
+{
+
+}
+
+//Tamanho da Fila
+void tamanhoFila ( )
+{
+
+}
+
+
+//Libera Fila
+void liberaFila ( ) 
+{
+
+}
+
+//Printa Fila
+void printaFila ( ) 
+{
+
+}
