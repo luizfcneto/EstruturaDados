7f808011f9a6b2dc2d15c5848c30f73932bc9053
Luiz Fernando <luizfcneto123@gmail.com>
2017-05-30 13:15:56 -0300

    *Declarando funcoes e estruturas

--- Filas/Dinamica/listaDinamica.h
+++ Filas/Dinamica/listaDinamica.h
@@ -4,3 +4,67 @@ Descrição:	Implementação e declaração das funções utilizadas em listaDin
 
 */
 
+//Estrutura Aluno 	(irei implementar apenas com matricula)
+struct tAluno
+{
+	int matricula;
+	// char nome[50];
+	// float notas[3];
+
+}	taluno;
+
+struct tElemento
+{
+	struct tElemento proximo;
+	taluno dado;
+}
+
+typedef struct tElemento* tRaiz;
+
+tRaiz* criaFila ()
+{
+	tRaiz *novo;
+	novo = ( tRaiz* ) malloc ( sizeof ( tRaiz ) );
+	if ( novo == NULL )	printf ("ERRO - Raiz Invalida, nao criada com sucesso! \n"); 
+
+	return novo;
+}
+
+//tamanho da Fila
+int tamanhoFila ( tRaiz* rfp )
+{
+
+
+}
+
+//fila Vazia
+int filaVazia ( tRaiz* rfp )
+{
+
+}
+
+//fila Cheia (Não faz sentido ter, porque é alocado sempre um elemento novo ao ser adicionado)
+
+//Libera Fila
+void liberaFila ( tRaiz* rfp )
+{
+
+}
+
+//Adiciona Elemento
+void adicionaElemento ( tRaiz* rfp, taluno* rap )
+{
+
+}
+
+//Remove Elemento
+void removeElemento ( tRaiz* rfp, taluno* rap )
+{
+
+}
+
+//Printa Fila
+void printaFila ( tRaiz* rfp )
+{
+	
+}
