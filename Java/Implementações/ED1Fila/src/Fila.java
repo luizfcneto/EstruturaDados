95db93f7edbca6ba7ad92e1efae49e7af7c2952e
luizfcneto <luizfcneto123@gmail.com>
2018-04-03 13:26:04 -0300

    *Estrutura de Fila circular de inteiros concluida

--- Java/Implementações/ED1Fila/src/Fila.java
+++ Java/Implementações/ED1Fila/src/Fila.java
@@ -7,46 +7,50 @@
  *	Atributos:
  *		int tamanhoFila;
  *		int inicioFila;
+ *		int fimFila;			//posicao em que iremos adicionar o proximo elemento na fila, quando chegar ao fim da fila deve retornar a posicao a partir de 0 (circular) formula: fim = ( inicioFila + quantidadeNaFila ) % tamanhoFila
  *		Vector vetorFila;		// fila estática -> usa-se vetor
  *		int quantidadeNaFila;
  *	
  *	Métodos: 
  * 		verificarCheia()
  * 		verificarVazia()
- * 		removerFinalDaFila()
+ * 		removeInicioDaFila()
  * 		adicionarNaFila()
  * 
  * 
  */
 
-import java.util.Vector;
+//import java.util.Vector;
 
 public class Fila {
 	int tamanhoFila;
 	int inicioFila;
+	int fimFila;
 	int quantidadeFila;
-	Vector vetorFila;
+	int[] vetorFila;
 	
 	//Construtor com tamanho
 	public Fila(int tamanho) {
 		this.tamanhoFila = tamanho;
-		this.vetorFila = new Vector( this.tamanhoFila );
+		this.vetorFila = new int[this.tamanhoFila];
 		this.quantidadeFila = 0;
 		this.inicioFila = 0;
+		this.fimFila = 0;
 	}
 	
 	//Construtor default
 	public Fila() {
 		this.tamanhoFila = 10;
-		this.vetorFila = new Vector( 10 );
+		this.vetorFila = new int[10];
 		this.quantidadeFila = 0;
 		this.inicioFila = 0;
+		this.fimFila = 0;
 	}
-	
 	
 	//verifica se a fila ta vazia
 	public boolean verificaVazia() {
-		if ( this.inicioFila == 0 )
+		//como a fila é circular para verificar se ela está vazia eu tenho que verificar a quantidade de elementos q estao na fila e não a posicao final ou inicial
+		if ( this.quantidadeFila == 0 )
 			return true;
 		else
 			return false;
@@ -55,7 +59,8 @@ public class Fila {
 	
 	//verifica se a fila está cheia
 	public boolean verificaCheia() {
-		if ( this.inicioFila == this.tamanhoFila )
+		//como a fila é circular para verificar se ela está cheia devo verificar se a quantidade de elementos é a mesma que o tamanho da fila.
+		if ( this.quantidadeFila == this.tamanhoFila )
 			return true;
 		else 
 			return false;
@@ -64,19 +69,45 @@ public class Fila {
 	
 	//adicionar elemento na Fila (no final da fila)
 	public void adicionaNaFila( int elementoNovo ) {
+		if ( !verificaCheia() ) {
+			this.vetorFila[ this.fimFila ] = elementoNovo;
+			this.quantidadeFila++;
+			this.fimFila = (this.fimFila + 1) % this.tamanhoFila;
 		
+		} else {
+			System.out.println("Fila Cheia!");
+		}
+				
 	}
 	
 	//remove elemento da fila (no inicio)
-	public void removeFinalDaFila() {
+	public int removeInicioDaFila() {
+		int elemento;
 		
+		if ( !verificaVazia() ) {
+			elemento = this.vetorFila[ this.inicioFila ];
+			this.inicioFila = ( 1 + this.inicioFila ) % this.tamanhoFila;
+			this.quantidadeFila--;
+			return elemento;
+			
+		} else {
+			//Fila Vazia
+			System.out.println( "FILA VAZIA!" );
+			return 0;
+			
+		}
 		
 	}
 	
 	
 	//printa Fila apenas para visualização dos elementos dentro da Fila
 	public void imprimeFila() {
-		
+		int percorreFila = 0;
+		System.out.println( "__________________\t\tInicio Fila\t\t__________________" );
+		for ( percorreFila = 0; percorreFila < this.tamanhoFila; percorreFila++ ) {
+			System.out.println("Posicao: "+ percorreFila + " Elemento: " + this.vetorFila[ percorreFila ]);
+		}
+		System.out.println( "__________________\t\tFinal Fila\t\t__________________" );
 	}
 
 }
